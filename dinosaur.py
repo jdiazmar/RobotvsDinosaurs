d07f6fdfc6ed1b0685a21c6e326a40f82d8a09bd
Josmar Diaz <jdm.begreat28@gmail.com>
2022-04-23 15:18:14 -0400

    Class Setup Dino + Robot File

---
+++ dinosaur.py
@@ -0,0 +1,13 @@
+class Dinosaur:
+    def __init__(self):
+        self.name = ''
+        self.attack_power = 100
+        self.health = 100
+    
+    def __init__(self, name, attack_power):
+        self.name = self.name
+        self.attack_power()
+
+    def attack(self, robot):
+        pass
+    
