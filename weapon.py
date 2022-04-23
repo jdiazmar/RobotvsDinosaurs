15f052ea5d27cc7764dee1000482ce2e93baaf09
Josmar Diaz <jdm.begreat28@gmail.com>
2022-04-23 15:38:36 -0400

    The rest of the classes File

---
+++ weapon.py
@@ -0,0 +1,8 @@
+class Weapon:
+    def __init__(self):
+        self.weapon = ''
+        self.attack_power = 100
+
+    def __init__(self, weapon, attack_power):
+        self.weapon = self.weapon
+        
