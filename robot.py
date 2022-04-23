9ee476d79bcc4aae28fb15ba4079a4307cd29aef
Josmar Diaz <jdm.begreat28@gmail.com>
2022-04-23 15:42:57 -0400

    Dino and Robot Weapon Class File

--- robot.py
+++ robot.py
@@ -1,11 +1,11 @@
 class Robot:
     def __init__(self):
-        self.name = ''
+        self.name = 'Relentless'
         self.health = 100
         self.weapon = []
 
     def _init__(self, name):
-        self.name = self.name 
+        self.name = ''
         
     def attack(self, dinosaur):
         pass 
--- weapon.py
+++ weapon.py
@@ -1,8 +1,7 @@
 class Weapon:
     def __init__(self):
-        self.weapon = ''
+        self.weapon = 'Laser Gun'
         self.attack_power = 100
 
     def __init__(self, weapon, attack_power):
         self.weapon = self.weapon
-        
