0664c94191c09fed0dd6229158570486a00d9de8
Josmar Diaz <jdm.begreat28@gmail.com>
2022-04-24 19:49:15 -0400

    Testing Objects File

---
+++ main.py
@@ -0,0 +1,11 @@
+from weapon import Weapon 
+from robot import Robot 
+from dinosaur import Dinosaur
+
+my_robot = Robot()
+my_dino = Dinosaur()
+mass_weapon = Weapon()
+
+print(my_robot.name)
+print(my_dino.name)
+print(mass_weapon.weapon)
