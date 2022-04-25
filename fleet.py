37a22187e1e8f2acf65aac121713029ffd668513
Josmar Diaz <jdm.begreat28@gmail.com>
2022-04-25 10:33:55 -0400

    Updated Robot and Fleet File

--- fleet.py
+++ fleet.py
@@ -3,8 +3,10 @@ from robot import Robot
 
 class Fleet:
     def __init__(self):
-        self.robots = [Robot.name, Robot.health, Robot.weapon]
+        pass
 
     
-    def create_fleet(self):
-     pass
+    def create_fleet(self, robot_passed):
+        self.robot_one = []
+        self.robot_two = []
+        self.robot_three = []
--- robot.py
+++ robot.py
@@ -3,7 +3,7 @@ from weapon import Weapon
 
 class Robot:
     def __init__(self):
-        self.name = 'Relentless'
+        self.name = ''
         self.health = 100
         self.weapon = Weapon 
         
