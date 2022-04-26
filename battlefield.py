2bca72ba7dc2231d5f8fad1840c7045789b3cd9b
Josmar Diaz <jdm.begreat28@gmail.com>
2022-04-25 22:22:13 -0400

    File

--- battlefield.py
+++ battlefield.py
@@ -12,28 +12,28 @@ class Battlefield:
 
     def run_game(self):
         self.display_welcome()
-        random.choice(Fleet.create_fleet, Herd.create_herd)
+        
         
 
     def display_welcome(self):
         self.display_welcome = 'Welcome to the ASTROWORLD! Where Winners Live and Losers Die!'
 
-    # def battle(self):
-    #     pass
+    def battle(self):
+        pass
 
-    # def dino_turn(self, dinosaur):
-    #     pass
+    def dino_turn(self, dinosaur):
+        pass
 
-    # def robo_turn(self, robot):
-    #     pass
+    def robo_turn(self, robot):
+        pass
 
-    # def show_dino_opponent_options(self):
-    #     pass
+    def show_dino_opponent_options(self):
+        pass
 
-    # def show_robo_opponent_options(self):
-    #     pass
+    def show_robo_opponent_options(self):
+        pass
 
-    # def display_winners(self):
-    #     pass 
+    def display_winners(self):
+        pass 
 
     
